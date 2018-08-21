6c55b5b3ffa23500e38242245d7f0daf1f76c891
Mo Berra <moses.r.berra@gmail.com>
2018-08-20 20:17:44 -0500

    proper unittest for homepage url, added homepage url mapping with dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_should_fail_bad_math(self):
-        self.assertEqual(1 + 1, 3)
+    def test_should_resolve_root_url_to_home_page_view(self):
+        found_homepage = resolve('/')
+        self.assertEqual(found_homepage.func, home_page)
