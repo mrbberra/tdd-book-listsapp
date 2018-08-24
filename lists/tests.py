5caf7830b2d7e8a67aee3c801c423ba3beffeea0
Mo Berra <moses.r.berra@gmail.com>
2018-08-23 20:24:57 -0500

    added model for list Items, plus db migrations for it

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
+from lists.models import Item
 
 class HomePageTest(TestCase):
 
@@ -19,3 +20,22 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'New list item'})
         self.assertIn('New list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+class ItemModelTest(TestCase):
+
+    def test_should_save_and_retrieve_items(self):
+        first_item = Item()
+        first_item.text = 'Here\'s the first one!'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Strike two'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'Here\'s the first one!')
+        self.assertEqual(second_saved_item.text, 'Strike two')
