51237b485090342d6782c13b15b1df39e1e08c08
Mo Berra <moses.r.berra@gmail.com>
2018-08-25 14:39:58 -0500

    rewrote FTs to use retry loop for time_sleeps to wait for selenium

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,11 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
 import unittest
+
+MAX_WAIT = 5
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -12,10 +15,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_table(self,row_text_expected):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text_expected, [row.text for row in rows])
+    def wait_for_row_in_table(self,row_text_expected):
+        end_time = time.time() + MAX_WAIT
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text_expected, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() > end_time:
+                    raise e
+                time.sleep(.25)
 
     def test_should_start_list_and_retreive_it_later(self):
         # JJ accesses the new To-Do list website
@@ -34,16 +45,14 @@ class NewVisitorTest(LiveServerTestCase):
         input_box.send_keys('Measure bed dimensions')
         input_box.send_keys(Keys.ENTER)
         # The page updates and JJ now sees a new list with her first to-do
-        time.sleep(1)
-        self.check_for_row_in_table('1: Measure bed dimensions')
+        self.wait_for_row_in_table('1: Measure bed dimensions')
         # JJ can still add more items. She adds a second item to her list
         input_box = self.browser.find_element_by_id('id_new_item')
         input_box.send_keys('Go to home depot')
         input_box.send_keys(Keys.ENTER)
         # The second item appears once the page updates, and the first remains
-        time.sleep(1)
-        self.check_for_row_in_table('1: Measure bed dimensions')
-        self.check_for_row_in_table('2: Go to home depot')
+        self.wait_for_row_in_table('1: Measure bed dimensions')
+        self.wait_for_row_in_table('2: Go to home depot')
         # JJ doesn't know how to access the list again from her other computer
         # She sees a new URL has been generated, and the site explains it
         self.fail('TODO: Finish Test')
