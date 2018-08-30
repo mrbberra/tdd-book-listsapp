fcaccb5979a4f1234ea72b8ff084ff3324e9ec7d
Mo Berra <moses.r.berra@gmail.com>
2018-08-29 23:06:42 -0500

    Added single url for viewing a list, refactored some tests

--- lists/tests.py
+++ lists/tests.py
@@ -8,10 +8,6 @@ from lists.models import Item
 
 class HomePageTest(TestCase):
 
-    def test_should_resolve_root_url_to_home_page_view(self):
-        found_homepage = resolve('/')
-        self.assertEqual(found_homepage.func, home_page)
-
     def test_should_use_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
@@ -27,19 +23,10 @@ class HomePageTest(TestCase):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_should_redirect_after_POST_request(self):
+    def test_should_redirect_after_POST(self):
         response = self.client.post('/', data={'item_text': 'New list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
-
-    def test_should_show_all_list_items(self):
-        Item.objects.create(text='firstly, an item')
-        Item.objects.create(text='secondly, another item')
-
-        response = self.client.get('/')
-
-        self.assertIn('firstly, an item', response.content.decode())
-        self.assertIn('secondly, another item', response.content.decode())
+        self.assertEqual(response['location'], '/lists/only-list-that-exists/')
 
 class ItemModelTest(TestCase):
 
@@ -59,3 +46,14 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'Here\'s the first one!')
         self.assertEqual(second_saved_item.text, 'Strike two')
+
+class ListViewTest(TestCase):
+
+    def test_should_display_all_items(self):
+        Item.objects.create(text="First item")
+        Item.objects.create(text="Second item")
+
+        response = self.client.get('/lists/only-list-that-exists/')
+
+        self.assertContains(response, "First item")
+        self.assertContains(response, "Second item")
